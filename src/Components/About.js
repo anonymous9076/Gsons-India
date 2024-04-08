462321f5fdf230fa6883e79d5b3744bc192098a1
Tushar kumar <68018920+anonymous9076@users.noreply.github.com>
2024-04-08 09:41:29 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,7 +17,7 @@ function About() {
         </div>
         <div className='about-body'>
         <div className='about-img'>
-            <img  data-aos="fade-down-right" src='./images/ai-generated-8515044_1920.jpg' alt='...'></img>
+            <img  data-aos="fade-down-right" src='./Images/ai-generated-8515044_1920.jpg' alt='...'></img>
         </div>
         <div className='about-text'>
             <p data-aos='fade-left'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
