4a3c04711a347c06d7660f519d1b2b763ccb147c
anonymous9076 <vijaytushar915@gmail.com>
2024-04-26 00:10:22 +0530

    all set

--- src/Components/Catelog.js
+++ src/Components/Catelog.js
@@ -1,42 +1,49 @@
 import React, { useEffect } from 'react'
 import './Catelog.css'
 import AOS from 'aos'
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 
 function Catelog() {
 
-  const light ='https://res.cloudinary.com/djnjmmgu8/image/upload/v1712740209/pdfs/RATAIL_ALL_ITEMS_LIST_yvtrtg.pdf';
-  const switches ='https://res.cloudinary.com/djnjmmgu8/image/upload/v1712739498/pdfs/GSONS_SWITCHES_ogrms7.pdf';
+  const light = 'https://res.cloudinary.com/djnjmmgu8/image/upload/v1712740209/pdfs/RATAIL_ALL_ITEMS_LIST_yvtrtg.pdf';
+  const switches = 'https://res.cloudinary.com/djnjmmgu8/image/upload/v1712739498/pdfs/GSONS_SWITCHES_ogrms7.pdf';
   const wire = 'https://res.cloudinary.com/djnjmmgu8/image/upload/v1713946774/pdfs/NO_PRICE_WIRE_ljn4se.pdf';
-
+  const concield = 'https://res.cloudinary.com/djnjmmgu8/image/upload/v1714043155/pdfs/GSONS_CONCIELD_LIGHTS_compressed_1_tymgbi.pdf';
+  const gate = 'https://res.cloudinary.com/djnjmmgu8/image/upload/v1714043426/pdfs/GATE_NO_PRICE_yfl0z7.pdf';
   const openPdf = (pdf) => {
-  
-      const atag = document.createElement('a')
-      atag.href = pdf
-      document.body.appendChild(atag)
-      atag.click()
-      atag.remove()
+
+    const atag = document.createElement('a')
+    atag.href = pdf
+    document.body.appendChild(atag)
+    atag.click()
+    atag.remove()
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init({
-     
+
     })
-},[])
+  }, [])
   return (
     <div className='catelog'>
       <div className='cat-left'>
         <div className='cat-heading' data-aos="fade-down">Our Catelogs</div>
         <div className='cat-pdfs'>
-        <div  data-aos="fade-down-right" onClick={()=>openPdf(wire)}> Wires 
-        <div id='wire'></div>
-        </div>
-        <div  data-aos="fade-down" onClick={()=>openPdf(switches)}>Switches 
-        <div id='switch'></div>
-        </div>
-        <div  data-aos="fade-down-left" onClick={()=>openPdf(light)}>Lights 
-          <div id='light'></div>
-        </div>
+          <div data-aos="fade-down-right" onClick={() => openPdf(wire)}> Wires
+            <div id='wire'></div>
+          </div>
+          <div data-aos="fade-down" onClick={() => openPdf(switches)}>Switches
+            <div id='switch'></div>
+          </div>
+          <div data-aos="fade-down-left" onClick={() => openPdf(light)}>Lights
+            <div id='light'></div>
+          </div>
+          <div data-aos="fade-down-left" onClick={() => openPdf(concield)}>Concield
+            <div id='concield'></div>
+          </div>
+          <div data-aos="fade-down-left" onClick={() => openPdf(gate)}>Gate Lamps
+            <div id='gate'></div>
+          </div>
         </div>
       </div>
       <div className='cat-right'>
